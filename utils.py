aedb717931e0e7305c2ea96b80a44b067da35ba7
Martin Maluenda <48397267+mmaluendap@users.noreply.github.com>
2024-05-09 00:24:13 -0400

    refactor: remove unnecesary int conversion

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ def get_rect_area_panels_count_and_fig(panel_dim1, panel_dim2, area_dim1, area_d
     area_height = min(area_dim1, area_dim2)
     area_width = max(area_dim1, area_dim2)
 
-    x_panel_count = int(area_width // panel_width)
-    y_panel_count = int(area_height // panel_height)
-    rotated_panels_y_count = int(area_height // panel_width)
-    rotated_panels_x_count = int(area_width % panel_width) // panel_height
+    x_panel_count = area_width // panel_width
+    y_panel_count = area_height // panel_height
+    rotated_panels_y_count = area_height // panel_width
+    rotated_panels_x_count = area_width % panel_width // panel_height
 
     total_panel_count = (
         x_panel_count * y_panel_count + rotated_panels_y_count * rotated_panels_x_count
