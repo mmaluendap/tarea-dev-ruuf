3ac5af2ca44c886c9779f6cff2dd22bbc516a70e
Martin Maluenda <48397267+mmaluendap@users.noreply.github.com>
2024-05-08 22:54:32 -0400

    feat: calculate panels btn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from utils import get_rect_area_panels_count_and_fig
+
 st.title('¿Cuántos paneles caben?')
 
 st.write(
@@ -27,3 +29,13 @@ with col1:
 
 with col2:
     panel_dim2 = st.number_input('Largo', value=0.0, key='panel_dim2_input')
+
+st.text('')
+if st.button('Calcular cuántos paneles caben', type='primary'):
+    panels_count, fig = get_rect_area_panels_count_and_fig(
+        panel_dim1, panel_dim2, area_dim1, area_dim2
+    )
+
+    st.write(f'### Caben {panels_count} paneles')
+
+    st.pyplot(None)
